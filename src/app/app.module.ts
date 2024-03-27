b0c9be320719c76ef238fd49bb7c813b0f6c9227
Farith Adnan <22324133+farithadnan@users.noreply.github.com>
2024-03-27 15:33:18 +0800

    Add TopNavbarComponent and assign it to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavbarComponent } from "./shared/menu/top-navbar/top-navbar.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavbarComponent,
   ],
   imports: [
     BrowserModule,
