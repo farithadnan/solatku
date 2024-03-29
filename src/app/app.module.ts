da6de698237e67576e3323fa620f68ce58974c9a
Farith Adnan <22324133+farithadnan@users.noreply.github.com>
2024-03-29 13:51:56 +0800

    Add ngx-toastr for displaying toast notifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { TuiIslandModule } from '@taiga-ui/kit';
 import { TuiNavigationModule } from "@taiga-ui/experimental";
 import { PrayerTimeBaseComponent } from './pages/prayer-time-base/prayer-time-base.component';
 import { NextPrayerInfoComponent } from './pages/prayer-time-base/next-prayer-info/next-prayer-info.component';
+import { ToastrModule } from 'ngx-toastr';
+import { HttpClientModule } from '@angular/common/http';
 
 const TUI_MODULES = [
   TuiRootModule,
@@ -30,9 +32,15 @@ const TUI_MODULES = [
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     TUI_MODULES,
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+    }),
 ],
   providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}],
   bootstrap: [AppComponent]
