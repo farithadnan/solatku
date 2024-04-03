7344fdbdd327d060a463267022cb4d48ad6d693b
Farith Adnan <22324133+farithadnan@users.noreply.github.com>
2024-04-04 00:48:15 +0800

    Add date enum - Add enum for IslamicMonth - Add enum for gregorianMonth

---
+++ src/app/shared/enums/date.enum.ts
@@ -0,0 +1,29 @@
+export enum IslamicMonth {
+  Muharam = "Muharam",
+  Safar = "Safar",
+  Rabiulawal = "Rabiulawal",
+  Rabiulakhir = "Rabiulakhir",
+  Jamadilawal = "Jamadilawal",
+  Jamadilakhir = "Jamadilakhir",
+  Rejab = "Rejab",
+  Syaaban = "Syaaban",
+  Ramadhan = "Ramadhan",
+  Syawal = "Syawal",
+  Zulkaedah = "Zulkaedah",
+  Zulhijah = "Zulhijah",
+}
+
+export enum GregorianMonth {
+  January = "Jan",
+  Febuary = "Feb",
+  March =  "Mar",
+  April = "Apr",
+  May = "May",
+  June = "Jun",
+  July = "Jul",
+  August = "Aug",
+  September = "Sep",
+  October = "Oct",
+  November = "Nov",
+  December = "Dec"
+}
