d1fe2c5c119a0deddcdb64a417c35349695e984a
Farith Adnan <22324133+farithadnan@users.noreply.github.com>
2024-04-04 00:50:03 +0800

    Add logic to get prayer times via zone code

--- src/app/pages/prayer-time-base/prayer-time-base.component.ts
+++ src/app/pages/prayer-time-base/prayer-time-base.component.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { PrayerTime, Solat } from 'src/app/shared/interfaces/solat.model';
 import { SolatService } from 'src/app/shared/services/solat.service';
 
 @Component({
@@ -9,11 +12,36 @@ import { SolatService } from 'src/app/shared/services/solat.service';
 export class PrayerTimeBaseComponent implements OnInit {
   chosenZone: string = 'WLY01';
   chosenDist: string = 'Kuala Lumpur';
+  monthlyData!: Solat;
+  todayPrayerTimes!: PrayerTime;
 
-  constructor(private solatApi: SolatService) {}
+  constructor(private solatApi: SolatService, private toastr: ToastrService) {}
 
   ngOnInit(): void {
     this.checkLocalStorage();
+
+    this.solatApi.getPrayerTimeByCode(this.chosenZone).subscribe({
+      next: (res) => {
+        this.monthlyData = res;
+        this.todayPrayerTimes = this.solatApi.getPrayerTimeViaDate(res);
+      },
+      error: (error) => {
+        console.error('Error:', error);
+
+        if (error instanceof HttpErrorResponse) {
+          if (error.status === 404) {
+            this.toastr.error('Zone code not found');
+            throw error;
+          }
+
+          if (error.status === 500) {
+            this.toastr.error('Internal server error');
+            throw error;
+          }
+          this.toastr.error('An unexpected error occurred');
+        }
+      },
+    })
   }
 
   /**
