774f7436e61b5274e15170a489f1134e442ed137
Farith Adnan <22324133+farithadnan@users.noreply.github.com>
2024-04-08 16:57:27 +0800

    Add TodayPrayerInfoComponent to display today's prayer information

---
+++ src/app/pages/prayer-time-base/today-prayer-info/today-prayer-info.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-today-prayer-info',
+  templateUrl: './today-prayer-info.component.html',
+  styleUrls: ['./today-prayer-info.component.less']
+})
+export class TodayPrayerInfoComponent {
+
+}
