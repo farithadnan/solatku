c9ce5b80a25b3ad5f627c53b23336f7c39fb4b49
Farith Adnan <22324133+farithadnan@users.noreply.github.com>
2024-04-08 16:18:09 +0800

    Update HTML template for next-prayer-info.component.html

--- src/app/pages/prayer-time-base/next-prayer-info/next-prayer-info.component.ts
+++ src/app/pages/prayer-time-base/next-prayer-info/next-prayer-info.component.ts
@@ -1,5 +1,5 @@
-import { ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subscription, map, timer } from 'rxjs';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { filter } from 'rxjs';
 import { IslamicMonth } from 'src/app/shared/enums/date.enum';
 import { NextPrayerInfo, PrayerTime, Solat } from 'src/app/shared/interfaces/solat.model';
 import { DateFilterService } from 'src/app/shared/services/date-filter.service';
@@ -11,29 +11,44 @@ import { SolatService } from 'src/app/shared/services/solat.service';
   styleUrls: ['./next-prayer-info.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NextPrayerInfoComponent implements OnInit, OnDestroy {
-  private subscription: Subscription = new Subscription();
-  countdown$!: Observable<number>;
-  hours: string = '';
-  minutes: string = '';
-  seconds: string = '';
-  showHours: boolean = false;
-  showMinutes: boolean = false;
-
+export class NextPrayerInfoComponent implements OnChanges {
   @Input() prayerTimes!: PrayerTime;
   @Input() monthlyTimes!: Solat;
 
   todayPrayers: NextPrayerInfo[] = [];
   nextPrayer!: NextPrayerInfo;
 
-  constructor(private solatApi: SolatService, private dateFilter: DateFilterService) {
+  nextPrayerName: string = '';
+  nextPrayerInSeconds: number = 0;
+
+  constructor(private solatApi: SolatService,
+              private dateFilter: DateFilterService) {
   }
 
-  ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges): void {
+    if ('prayerTimes' in changes && 'monthlyTimes' in changes) {
+      this.calculateNextPrayer();
+    }
   }
 
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+  /**
+   * Dissect prayer times from PrayerTime object.
+   * @param originData todays prayer times including zone & hijri date.
+   * @returns a list of todays prayer info.
+   */
+  private getPrayerTimes(originData: PrayerTime) {
+    const fajrTime = this.dateFilter.epochToJsDate(originData.fajr);
+    const imsakTime = new Date(fajrTime.getTime() - 10 * 60000);
+
+    return [
+      { name: 'Imsak', time: imsakTime },
+      { name: 'Subuh', time: fajrTime },
+      { name: 'Syuruk', time: this.dateFilter.epochToJsDate(originData.syuruk) },
+      { name: 'Zohor', time: this.dateFilter.epochToJsDate(originData.dhuhr) },
+      { name: 'Asar', time: this.dateFilter.epochToJsDate(originData.asr) },
+      { name: 'Maghrib', time: this.dateFilter.epochToJsDate(originData.maghrib) },
+      { name: 'Isyak', time: this.dateFilter.epochToJsDate(originData.isha) }
+    ];
   }
 
   /**
@@ -41,22 +56,84 @@ export class NextPrayerInfoComponent implements OnInit, OnDestroy {
    * @param data todays prayer times including zone & hijri date.
    * @returns the next prayer names (string).
    */
-  getNextPrayerName(data: PrayerTime) {
+  calculateNextPrayer() {
+    if (!this.prayerTimes || !this.monthlyTimes) {
+      return 'Data is Empty';
+    }
+
     const now = new Date();
-    this.todayPrayers = this.getPrayerTimes(data);
+    this.todayPrayers = this.getPrayerTimes(this.prayerTimes);
     const nextPrayerList = this.filterUpcomingPrayers(this.todayPrayers, now);
 
     if (nextPrayerList.length < 1) {
       // Fetch tomorrow's prayer times
       this.nextPrayer = this.getUpcomingFajrTimes(now, this.monthlyTimes);
-      return this.nextPrayer.name;
+    } else {
+      // Sort future by closest to the current time.
+      this.nextPrayer = this.sortByTime(nextPrayerList)[0];
     }
 
-    // Sort future by closest to the current time.
-    this.nextPrayer = this.sortByTime(nextPrayerList)[0];
+    this.nextPrayerInSeconds = this.getDurationInSeconds(this.nextPrayer.time);
     return this.nextPrayer.name
   }
 
+  /**
+ * Filter upcoming prayer info for today.
+ * @param todayPrayerTimes list of today's prayer times.
+ * @param currentTime current dateTime.
+ * @returns a list of the next upcoming prayer for today.
+ */
+  filterUpcomingPrayers(todayPrayerTimes: NextPrayerInfo[], currentTime: Date) {
+    return todayPrayerTimes.filter(prayer => {
+      return (prayer.time > currentTime) && prayer.name !== 'Imsak' && prayer.name !== 'Syuruk'
+    });
+  }
+
+  /**
+ * Sort via the closest upcoming prayer to the current time.
+ * @param todayPrayerTimes list of today's prayer times.
+ * @returns a sorted prayer times.
+ */
+  private sortByTime(todayPrayerTimes: NextPrayerInfo[]) {
+    return todayPrayerTimes.sort((now, nxt) => now.time.getTime() - nxt.time.getTime());
+  }
+
+  /**
+   * Get next fajr prayer info.
+   * @param todayDate current dateTime.
+   * @param monthlyTimes current month data of prayer time info.
+   * @returns The next fajr prayer time info.
+   */
+  private getUpcomingFajrTimes(todayDate: Date, monthlyTimes: Solat) {
+    let tomorrow = todayDate;
+    tomorrow.setDate(todayDate.getDate() + 1);
+    const tomorrowTimes = this.solatApi.getPrayerTimeViaDate(monthlyTimes, tomorrow);
+    const subuh: NextPrayerInfo = {
+      name: "Subuh",
+      time: this.dateFilter.epochToJsDate(tomorrowTimes.fajr)
+    }
+    return subuh;
+  }
+
+  /**
+ * Get the duration of the next prayer in seconds.
+ * @param targetDate a target date to calculate the duration.
+ * @returns a duration in seconds.
+ */
+  private getDurationInSeconds(targetDate: Date) {
+    const currentDate = new Date();
+    const milliseconds = targetDate.getTime() - currentDate.getTime();
+    const seconds = Math.floor(milliseconds / 1000);
+    return seconds;
+  }
+
+  /**
+   * Get the name of the district.
+   * @returns the name of the district.
+   */
+  getDistrictName() {
+    return localStorage.getItem('district');
+  }
 
   /**
    * Get and format hijri date.
@@ -77,99 +154,4 @@ export class NextPrayerInfoComponent implements OnInit, OnDestroy {
   getTodaysDate() {
     return new Date()
   }
-
-
-  getCountdown() {
-    const startTime = new Date().getTime();
-    const endTime = this.nextPrayer.time.getTime();
-    const countdownDuration = Math.floor((endTime - startTime) / 1000);
-
-    this.countdown$ = timer(0, 1000).pipe(
-      map(() => {
-        const currentTime = new Date();
-        const remainingTime = Math.floor((endTime - currentTime.getTime()) / 1000);
-
-        if (remainingTime <= 0) {
-          this.subscription.unsubscribe();
-          return 0;
-        }
-
-        this.showHours = remainingTime >= 3600;
-        this.showMinutes = remainingTime >= 60;
-
-        return remainingTime;
-      })
-    );
-
-    this.subscription = this.countdown$.subscribe(time => {
-      this.hours = this.formatNumber(Math.floor(time / 3600));
-      this.minutes = this.formatNumber(Math.floor((time % 3600) / 60));
-      this.seconds = this.formatNumber(time % 60);
-    });
-
-  }
-
-  // Helper function to format numbers to two digits with leading zero
-  private formatNumber(value: number): string {
-    return value < 10 ? '0' + value : '' + value;
-  }
-
-  /**
-   * Dissect prayer times from PrayerTime object.
-   * @param originData todays prayer times including zone & hijri date.
-   * @returns a list of todays prayer info.
-   */
-  getPrayerTimes(originData: PrayerTime) {
-    const fajrTime = this.dateFilter.epochToJsDate(originData.fajr);
-    const imsakTime = new Date(fajrTime.getTime() - 10 * 60000);
-
-    return [
-      { name: 'Imsak', time: imsakTime },
-      { name: 'Subuh', time: fajrTime },
-      { name: 'Syuruk', time: this.dateFilter.epochToJsDate(originData.syuruk) },
-      { name: 'Zohor', time: this.dateFilter.epochToJsDate(originData.dhuhr) },
-      { name: 'Asar', time: this.dateFilter.epochToJsDate(originData.asr) },
-      { name: 'Maghrib', time: this.dateFilter.epochToJsDate(originData.maghrib) },
-      { name: 'Isyak', time: this.dateFilter.epochToJsDate(originData.isha) }
-    ];
-  }
-
-  /**
-   * Filter upcoming prayer info for today.
-   * @param todayPrayerTimes list of today's prayer times.
-   * @param currentTime current dateTime.
-   * @returns a list of the next upcoming prayer for today.
-   */
-  filterUpcomingPrayers(todayPrayerTimes: NextPrayerInfo[], currentTime: Date) {
-    return todayPrayerTimes.filter(prayer => {
-      return (prayer.time > currentTime) && prayer.name !== 'Imsak' && prayer.name !== 'Syuruk'
-    });
-  }
-
-  /**
-   * Sort via the closest upcoming prayer to the current time.
-   * @param todayPrayerTimes list of today's prayer times.
-   * @returns a sorted prayer times.
-   */
-  sortByTime(todayPrayerTimes: NextPrayerInfo[]) {
-    return todayPrayerTimes.sort((now, nxt) => now.time.getTime() - nxt.time.getTime());
-  }
-
-  /**
-   * Get next fajr prayer info.
-   * @param todayDate current dateTime.
-   * @param monthlyTimes current month data of prayer time info.
-   * @returns The next fajr prayer time info.
-   */
-  getUpcomingFajrTimes(todayDate: Date, monthlyTimes: Solat) {
-    let tomorrow = todayDate;
-    tomorrow.setDate(todayDate.getDate() + 1);
-    const tomorrowTimes = this.solatApi.getPrayerTimeViaDate(monthlyTimes, tomorrow);
-    const subuh: NextPrayerInfo = {
-      name: "Subuh",
-      time: this.dateFilter.epochToJsDate(tomorrowTimes.fajr)
-    }
-    return subuh;
-  }
-
 }
