5c2bb7750ad383894a401cc914bf5db5d84da9f2
Farith Adnan <22324133+farithadnan@users.noreply.github.com>
2024-04-09 16:53:50 +0800

    Update app.module.ts to import TuiTableModule, TuiSvgModule, TuiSelectModule, TuiDataListModule, TuiDataListWrapperModule, and TuiTextfieldControllerModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavbarComponent } from "./shared/menu/top-navbar/top-navbar.component";
+import { PrayerTimeBaseComponent } from './pages/prayer-time-base/prayer-time-base.component';
+import { NextPrayerInfoComponent } from './pages/prayer-time-base/next-prayer-info/next-prayer-info.component';
+import { NotificationComponent } from './shared/notification/notification.component';
+import { PrayerTimeTableComponent } from './pages/prayer-time-base/prayer-time-table/prayer-time-table.component';
+import { ZoneSwitcherComponent } from './shared/dialogs/zone-switcher/zone-switcher.component';
 
 import { ToastrModule } from 'ngx-toastr';
 import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
-import { TuiRootModule, TuiDialogModule, TuiButtonModule, TUI_SANITIZER, TuiNotificationModule, TuiSvgModule, TuiHintModule } from "@taiga-ui/core";
-import { TuiIslandModule } from '@taiga-ui/kit';
+import { TuiRootModule, TuiDialogModule, TuiButtonModule, TUI_SANITIZER,
+         TuiNotificationModule, TuiSvgModule, TuiHintModule, TuiDataListModule,
+         TuiTextfieldControllerModule} from "@taiga-ui/core";
+import { TuiDataListWrapperModule, TuiIslandModule, TuiSelectModule } from '@taiga-ui/kit';
 import { TuiTableModule } from '@taiga-ui/addon-table';
 import { TuiNavigationModule } from "@taiga-ui/experimental";
-import { PrayerTimeBaseComponent } from './pages/prayer-time-base/prayer-time-base.component';
-import { NextPrayerInfoComponent } from './pages/prayer-time-base/next-prayer-info/next-prayer-info.component';
 
 import { EpochToDatePipe } from './shared/pipes/epoch-to-date.pipe';
 import { CountdownPipe } from './shared/pipes/countdown.pipe';
-import { NotificationComponent } from './shared/notification/notification.component';
-import { PrayerTimeTableComponent } from './pages/prayer-time-base/prayer-time-table/prayer-time-table.component';
 
 const TUI_MODULES = [
   TuiRootModule,
@@ -32,6 +35,10 @@ const TUI_MODULES = [
   TuiTableModule,
   TuiSvgModule,
   TuiHintModule,
+  TuiSelectModule,
+  TuiDataListModule,
+  TuiDataListWrapperModule,
+  TuiTextfieldControllerModule,
 ];
 
 @NgModule({
@@ -42,11 +49,14 @@ const TUI_MODULES = [
     NextPrayerInfoComponent,
     PrayerTimeTableComponent,
     NotificationComponent,
+    ZoneSwitcherComponent,
     EpochToDatePipe,
     CountdownPipe,
   ],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
