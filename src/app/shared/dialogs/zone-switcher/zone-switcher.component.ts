0421a96e04e1681681f98875a26cbf750cf10ca5
Farith Adnan <22324133+farithadnan@users.noreply.github.com>
2024-04-22 14:00:52 +0800

    Update ZoneSwitcherComponent to include size and placeholder properties

--- src/app/shared/dialogs/zone-switcher/zone-switcher.component.ts
+++ src/app/shared/dialogs/zone-switcher/zone-switcher.component.ts
@@ -10,6 +10,8 @@ import { POLYMORPHEUS_CONTEXT } from '@tinkoff/ng-polymorpheus';
 })
 export class ZoneSwitcherComponent {
   value: number | null = null;
+  size: 's' | 'm' | 'l' = 'm';
+  placeholder = 'Pilih Zon Anda';
   items = [10, 50, 100];
   constructor(@Inject(TuiDialogService) private readonly dialogs: TuiDialogService,
               @Inject(POLYMORPHEUS_CONTEXT) private readonly context: TuiDialogContext<number, number>) {}
