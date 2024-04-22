fc037149d0f81607964b63c43cc20da6640c8a9e
Farith Adnan <22324133+farithadnan@users.noreply.github.com>
2024-04-22 16:59:20 +0800

    Update ZoneService to include groupZoneByState and getJakimCode methods

--- src/app/shared/services/zone.service.ts
+++ src/app/shared/services/zone.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { Zone } from "../interfaces/zone.model";
+import { GroupZone, Zone, Daerah } from "../interfaces/zone.model";
 import { Observable, catchError, throwError } from "rxjs";
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { ApiService } from "./api.service";
@@ -18,9 +18,9 @@ export class ZoneService {
    * @param zoneCode [optional] The zone code to get the zone information.
    * @returns a promise of the zone information.
    */
-  getZones(zoneCode?: string): Observable<Zone>{
+  getZones(zoneCode?: string): Observable<Zone[]>{
     const url = zoneCode ? `${this.api.zoneUri}/${zoneCode}` : this.api.zoneUri;
-    return this.http.get<Zone>(url).pipe(
+    return this.http.get<Zone[]>(url).pipe(
       catchError((error: HttpErrorResponse): Observable<any> => {
         if (error.status === 404) {
           this.toastr.error('Provided zone code doesn\'t exist');
@@ -32,5 +32,43 @@ export class ZoneService {
         return throwError(() => error);
       })
     );
+  }
+
+  /**
+   * Group the zones by state.
+   * @param zones a list of zones to group by state.
+   * @returns a list of grouped zones by state.
+   */
+  groupZoneByState(zones: Zone[]): GroupZone[] {
+    const groupedData: { [key: string]: Daerah[] } = {};
+    zones.forEach(zone => {
+      if (!groupedData[zone.negeri]) {
+        groupedData[zone.negeri] = [];
+      }
+      groupedData[zone.negeri].push({ jakimCode: zone.jakimCode, name: zone.daerah });
+    });
+
+    return Object.entries(groupedData).map(([negeri, values]) => ({
+      negeri,
+      data: values,
+    }));
+  }
+
+  /**
+   * Get the JAKIM code for the district.
+   * @param district the district name to get the JAKIM code.
+   * @param zones a list of zones to get the JAKIM code.
+   * @returns the JAKIM code for the district.
+   */
+  getJakimCode(district: string, zones: GroupZone[]): string {
+    let jakimCode = '';
+    zones.forEach(zone => {
+      zone.data.forEach(daerah => {
+        if (daerah.name === district) {
+          jakimCode = daerah.jakimCode;
+        }
+      });
+    });
+    return jakimCode;
   }
 }
