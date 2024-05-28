65988b0245c414eedc55d02bffd42dbec49df2ce
Farith Adnan <22324133+farithadnan@users.noreply.github.com>
2024-05-28 12:51:12 +0800

    feat: Add language and theme selection to top navbar

--- src/app/shared/menu/top-navbar/top-navbar.component.ts
+++ src/app/shared/menu/top-navbar/top-navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslatorService } from '../../services/translator.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-top-navbar',
@@ -6,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top-navbar.component.less']
 })
 export class TopNavbarComponent implements OnInit{
+  open: boolean = false;
 
-  constructor() { }
+  constructor(private translator: TranslatorService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+  }
+
+  changeLanguage(event: any) {
+    const selectedLanguage = event.target.value;
+    this.translator.changeLanguage(selectedLanguage);
+
+    const title = this.translator.getTranslation('solatku.toastr.title.success');
+    const message = this.translator.getTranslation('solatku.toastr.zone_switcher_section.language_change_success_msg');
+    this.toastr.success(message, title);
+  }
+
+  changeTheme() {
+
   }
 }
