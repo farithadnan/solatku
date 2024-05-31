87f1d8f0b763f5b7e84f70555c30c222d1021125
Farith Adnan <22324133+farithadnan@users.noreply.github.com>
2024-06-01 01:17:23 +0800

    Add component to hold instruction for ios's installation

---
+++ src/app/shared/dialogs/ios-instruction/ios-instruction.component.ts
@@ -0,0 +1,11 @@
+import { Component, ChangeDetectionStrategy } from "@angular/core";
+
+@Component({
+  selector: 'app-ios-instruction',
+  templateUrl: './ios-instruction.component.html',
+  styleUrls: ['./ios-instruction.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class iosInstructionComponent {
+  constructor() {}
+}
