ee97ef4db266055066a942dd5e205435ac91a0d2
Farith Adnan <22324133+farithadnan@users.noreply.github.com>
2024-05-31 20:44:12 +0800

    Update script for handling env files

--- mynode.js
+++ mynode.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const dotenv = require('dotenv').config({path: 'src/.env'});
+const dotenv = require('dotenv').config({path: './src/.env'});
 
 const successColor = '\x1b[32m%s\x1b[0m';
 const checkSign = '\u{2705}';
@@ -27,12 +27,12 @@ const envFileContent = `export const environment = {
   };
 `;
 
-const targetPath = path.join(__dirname, './src/environments/environment.ts');
+const targetPath = path.join(__dirname, './src/environments/environment.prod.ts');
 fs.writeFile(targetPath, envFileContent, (err) => {
   if (err) {
     console.error(err);
     throw err;
   } else {
-    console.log(successColor, `${checkSign} Successfully generated environment.ts`);
+    console.log(successColor, `${checkSign} Successfully generated environment.prod.ts`);
   }
 });
